656fc8b33f87b42eb5fc1c78778f9d54b475b26d
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2020-01-30 17:11:43 +0300

    Initial commit

---
+++ src/main/java/com/mytests/micronaut/configProperties/beans/BeanWithPropertyAnnos.java
@@ -0,0 +1,35 @@
+package com.mytests.micronaut.configProperties.beans;
+
+import io.micronaut.context.annotation.Property;
+
+import javax.inject.Singleton;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 1/30/2020.
+ * Project: micronaut-config-properties
+ * *******************************
+ */
+@Singleton
+public class BeanWithPropertyAnnos {
+
+    @Property(name = "")
+    String p1;
+    String p2;
+
+    public String getP1() {
+        return p1;
+    }
+
+    public void setP1(String p1) {
+        this.p1 = p1;
+    }
+
+    public String getP2() {
+        return p2;
+    }
+
+    public void setP2(String p2) {
+        this.p2 = p2;
+    }
+}
