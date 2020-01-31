5f546918aa5445ef175ead1dba1ba8204973c45d
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2020-01-31 14:34:58 +0300

    Application class added

---
+++ src/main/java/com/mytests/micronaut/configProperties/Application.java
@@ -0,0 +1,15 @@
+package com.mytests.micronaut.configProperties;
+
+import io.micronaut.runtime.Micronaut;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 1/31/2020.
+ * Project: micronaut-config-properties
+ * *******************************
+ */
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
